fc6b1c5c4fd19e3ddc779ab206b6e1849198cf51
FranciscoBaglietto <fbagliettopiermattei@gmail.com>
2022-11-03 22:19:11 -0300

    s

--- index.js
+++ index.js
@@ -1,43 +1,65 @@
-//Entrega 1 = Clases
+// //Entrega 1 = Clases
 
-class Usuario {
-  constructor(nombre, apellido) {
+// class Usuario {
+//   constructor(nombre, apellido) {
+//     this.nombre = nombre;
+//     this.apellido = apellido;
+//     this.libros = [];
+//     this.mascotas = [];
+//   }
+
+//   getFullName() {
+//     return `Nombre: ${this.nombre} Apellido: ${this.apellido}`;
+//   }
+
+//   addMascotas(nombreMascota) {
+//     this.mascotas.push(nombreMascota);
+//   }
+
+//   countMascotas() {
+//     return this.mascotas.length;
+//   }
+
+//   addBook(nombre, autor) {
+//     this.libros.push({ nombre, autor });
+//   }
+
+//   getBookNames() {
+//     return this.libros.map((libro) => libro.nombre);
+//   }
+// }
+
+// const objeto1 = new Usuario("Francisco", "Baglietto");
+
+// console.log(objeto1.getFullName());
+
+// objeto1.addMascotas("Perro");
+// objeto1.addMascotas("Gato");
+// console.log(objeto1.countMascotas());
+
+// objeto1.addBook("Harry Potter", "Rowling");
+// objeto1.addBook("El Principito", "Antonie");
+
+// console.log(objeto1.getBookNames());
+
+// Entrega 2 = Manejo de Arquivos
+
+const fs = require("fs");
+
+class Contenedor {
+  constructor(nombre) {
     this.nombre = nombre;
-    this.apellido = apellido;
-    this.libros = [];
-    this.mascotas = [];
   }
 
-  getFullName() {
-    return `Nombre: ${this.nombre} Apellido: ${this.apellido}`;
-  }
+  save() {}
 
-  addMascotas(nombreMascota) {
-    this.mascotas.push(nombreMascota);
-  }
+  getById(id) {}
 
-  countMascotas() {
-    return this.mascotas.length;
-  }
+  getAll() {}
 
-  addBook(nombre, autor) {
-    this.libros.push({ nombre, autor });
-  }
+  deleteById(id) {}
 
-  getBookNames() {
-    return this.libros.map((libro) => libro.nombre);
-  }
+  deleteAll() {}
 }
 
-const objeto1 = new Usuario("Francisco", "Baglietto");
 
-console.log(objeto1.getFullName());
-
-objeto1.addMascotas("Perro");
-objeto1.addMascotas("Gato");
-console.log(objeto1.countMascotas());
-
-objeto1.addBook("Harry Potter", "Rowling");
-objeto1.addBook("El Principito", "Antonie");
-
-console.log(objeto1.getBookNames());
